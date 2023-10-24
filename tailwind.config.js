13c4918d28a9e992f905d0694ae5795662a97b88
Eol1881 <02f1881@gmail.com>
2023-10-24 18:04:59 +0300

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        pixelify: ['Pixelify', 'sans'],
+      },
+    },
   },
   plugins: [],
 };
