6e6be03b3ce99e475a527a33dd7592ed74189a7d
Eol1881 <02f1881@gmail.com>
2023-11-01 02:36:09 +0300

    refactor: convert list item component

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { ImageData } from '../api/types';
 import { trimImageName } from '../utils/trimImageName';
 import { formatDate } from '../utils/formatDate';
@@ -7,64 +7,48 @@ interface Props {
   imageData: ImageData;
 }
 
-interface State {
-  isHovered: boolean;
-  tooltipTimer: number;
-}
+export function ListItem(props: Props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const { imageData } = props;
+  let tooltipTimer: number;
 
-export class ListItem extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      isHovered: false,
-      tooltipTimer: 0,
-    };
-  }
+  const imageUrl = imageData.links ? imageData.links[0].href : '';
+  const imageTitle = imageData.data[0].title;
+  const itemId = imageData.data[0].nasa_id;
+  const center = imageData.data[0].center;
+  const dateCreatedRaw = imageData.data[0].date_created;
+  const dateCreatedFormatted = formatDate(new Date(dateCreatedRaw));
 
-  mouseEnterHandler = () => {
-    const newTimer = setTimeout(() => {
-      this.setState({ isHovered: true });
+  const mouseEnterHandler = () => {
+    tooltipTimer = setTimeout(() => {
+      setIsHovered(true);
     }, 200);
-    this.setState({ tooltipTimer: newTimer });
   };
 
-  mouseLeaveHandler = () => {
-    const { tooltipTimer } = this.state;
+  const mouseLeaveHandler = () => {
     clearTimeout(tooltipTimer);
-    this.setState({ isHovered: false });
+    setIsHovered(false);
   };
 
-  render() {
-    const { imageData } = this.props;
-    const { isHovered } = this.state;
+  return (
+    <div
+      key={itemId}
+      data-item-id={itemId}
+      className="relative cursor-pointer select-none rounded-md p-1 hover:bg-sky-200"
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+    >
+      {trimImageName(imageTitle)}
 
-    const imageUrl = imageData.links ? imageData.links[0].href : '';
-    const imageTitle = imageData.data[0].title;
-    const itemId = imageData.data[0].nasa_id;
-    const center = imageData.data[0].center;
-    const dateCreatedRaw = imageData.data[0].date_created;
-    const dateCreatedFormatted = formatDate(new Date(dateCreatedRaw));
-
-    return (
       <div
-        key={itemId}
-        data-item-id={itemId}
-        className="relative cursor-pointer select-none rounded-md p-1 hover:bg-sky-200"
-        onMouseEnter={this.mouseEnterHandler}
-        onMouseLeave={this.mouseLeaveHandler}
+        className={`tooltip ${
+          isHovered ? 'visible opacity-100' : 'invisible opacity-0'
+        }`}
       >
-        {trimImageName(imageTitle)}
-
-        <div
-          className={`tooltip ${
-            isHovered ? 'visible opacity-100' : 'invisible opacity-0'
-          }`}
-        >
-          <img src={imageUrl} alt="Image" className="h-32 w-32 object-cover" />
-          <p className="font-pixelify">Center: {center}</p>
-          <p className="font-pixelify">{dateCreatedFormatted}</p>
-        </div>
+        <img src={imageUrl} alt="Image" className="h-32 w-32 object-cover" />
+        <p className="font-pixelify">Center: {center}</p>
+        <p className="font-pixelify">{dateCreatedFormatted}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
