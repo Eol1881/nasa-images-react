ccfc1dd7dea18509a823ac91582574ab3ef2d746
Eol1881 <02f1881@gmail.com>
2023-11-01 02:37:02 +0300

    refactor: convert loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
-export class Loader extends React.Component {
-  render() {
-    return (
-      <div className="mt-4 space-y-2 rounded-lg bg-white px-2 py-3 text-black shadow-md sm:px-4">
-        Loading...
-      </div>
-    );
-  }
+export function Loader() {
+  return (
+    <div className="mt-4 space-y-2 rounded-lg bg-white px-2 py-3 text-black shadow-md sm:px-4">
+      Loading...
+    </div>
+  );
 }
