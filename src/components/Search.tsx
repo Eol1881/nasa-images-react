9329c37b7c261a774748b2e19f5b32dcf23ded03
Eol1881 <02f1881@gmail.com>
2023-11-01 03:27:05 +0300

    refactor: convert search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 interface Props {
   searchHandler: (searchQuery: string) => void;
@@ -6,71 +6,47 @@ interface Props {
   searchQuery: string;
 }
 
-interface State {
-  searchQuery: string;
-}
+export function Search(props: Props) {
+  const [searchInputValue, setSearchInputValue] = useState(props.searchQuery);
 
-export class Search extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      searchQuery: props.searchQuery,
-    };
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    if (prevProps.searchQuery !== this.props.searchQuery) {
-      this.setState({ searchQuery: this.props.searchQuery });
-    }
-  }
-
-  resetHandler = () => {
-    this.setState({ searchQuery: '' }, () => {
-      localStorage.removeItem('nasa-search-queue');
-      this.props.resetHandler();
-    });
+  const resetHandler = () => {
+    setSearchInputValue('');
+    localStorage.removeItem('nasa-search-queue');
+    props.resetHandler();
   };
 
-  searchHandler = () => {
-    localStorage.setItem('nasa-search-queue', this.state.searchQuery);
-    this.props.searchHandler(this.state.searchQuery);
+  const handleSearch = () => {
+    localStorage.setItem('nasa-search-queue', searchInputValue);
+    props.searchHandler(searchInputValue);
   };
 
-  render() {
-    return (
-      <div className="mt-4 space-y-4 rounded-lg bg-white px-2 py-3 text-center text-black shadow-md sm:px-4 sm:text-left">
-        <h1 className="select-none font-pixelify text-3xl font-bold">
-          🚀 NASA Images Viewer
-        </h1>
-        <div className="flex justify-stretch">
-          <input
-            id="search"
-            className="min-w-0 flex-grow rounded-s-md bg-slate-300 p-1 pl-2 font-pixelify backdrop-blur-lg"
-            value={this.state.searchQuery}
-            placeholder="type search keywords"
-            type="text"
-            onChange={(e) => {
-              this.setState({ searchQuery: e.target.value.trim() });
-            }}
-            onKeyDown={(e) => {
-              if (e.key !== 'Enter') return;
-              this.searchHandler();
-            }}
-          />
-          <button
-            className="button-red !rounded-none"
-            onClick={this.resetHandler}
-          >
-            Reset
-          </button>
-          <button
-            className="button-blue !rounded-s-none"
-            onClick={this.searchHandler}
-          >
-            Search
-          </button>
-        </div>
+  return (
+    <div className="mt-4 space-y-4 rounded-lg bg-white px-2 py-3 text-center text-black shadow-md sm:px-4 sm:text-left">
+      <h1 className="select-none font-pixelify text-3xl font-bold">
+        🚀 NASA Images Viewer
+      </h1>
+      <div className="flex justify-stretch">
+        <input
+          id="search"
+          className="min-w-0 flex-grow rounded-s-md bg-slate-300 p-1 pl-2 font-pixelify backdrop-blur-lg"
+          value={searchInputValue}
+          placeholder="type search keywords"
+          type="text"
+          onChange={(e) => {
+            setSearchInputValue(e.target.value.trim());
+          }}
+          onKeyDown={(e) => {
+            if (e.key !== 'Enter') return;
+            handleSearch();
+          }}
+        />
+        <button className="button-red !rounded-none" onClick={resetHandler}>
+          Reset
+        </button>
+        <button className="button-blue !rounded-s-none" onClick={handleSearch}>
+          Search
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
