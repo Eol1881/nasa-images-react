57c8f9ee041cb4f02811fdd1739744dd058a9049
Eol1881 <01f1881@gmail.com>
2023-11-09 21:33:35 +0300

    refactor: make extractErrorMessage method more readable

--- src/utils/extractErrorMessage.ts
+++ src/utils/extractErrorMessage.ts
@@ -1,14 +1,9 @@
 import { isRouteErrorResponse } from 'react-router-dom';
 
 export function extractErrorMessage(error: unknown): string {
-  if (isRouteErrorResponse(error)) {
-    return `${error.status} ${error.statusText}`;
-  } else if (error instanceof Error) {
-    return error.message;
-  } else if (typeof error === 'string') {
-    return error;
-  } else {
-    console.error(error);
-    return 'Unknown error';
-  }
+  if (isRouteErrorResponse(error)) return `${error.status} ${error.statusText}`;
+  if (error instanceof Error) return error.message;
+  if (typeof error === 'string') return error;
+  console.error(error);
+  return 'Unknown error';
 }
