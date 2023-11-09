fdc434e2a9d51eb137c5e48916b5a4e114503270
Eol1881 <01f1881@gmail.com>
2023-11-09 21:07:29 +0300

    fix: reconciliation

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from 'react-router-dom';
 import { ImageData } from '../api/types';
 import { ResultItem } from './ResultItem';
 import { useEffect, useState } from 'react';
+import { extractImageData } from '../utils/extractImageData';
 
 interface Props {
   imagesData: ImageData[];
@@ -26,8 +27,8 @@ export const ResultList: React.FC<Props> = ({ imagesData, shouldThrowError }) =>
       className={`relative w-full select-none space-y-2 md:block md:pr-4
       ${isLoading && 'pointer-events-none text-slate-400'} ${isDetailsOpened && 'hidden'}`}
     >
-      {imagesData.map((imageData, imageIndex) => (
-        <ResultItem key={imageIndex} imageData={imageData}></ResultItem>
+      {imagesData.map((imageData) => (
+        <ResultItem key={extractImageData(imageData).nasaId} imageData={imageData}></ResultItem>
       ))}
       <div
         className={`font-pixelify text-2xl text-red-700 ${!isNothingFound && 'invisible hidden'} ${
